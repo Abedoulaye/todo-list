a59b2c90194faf8973511babba4355d5e2ecce5f
Your Name <abedoulaye@DESKTOP-MB4JS6O.localdomain>
2026-06-24 14:46:30 -0400

    javascript functionality done

--- script.js
+++ script.js
@@ -10,5 +10,120 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
+
+/* input values shouldn't retain their values so we make this function, alongside with 
+toggling classList("hidden") and resetting currentTask whenver new tasks are made */
+
+function reset(){
+  addOrUpdateTaskBtn.innerText = "Add Task"
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+
+}
+
+const updateTaskContainer = () => {
+  tasksContainer.innerHTML = "";
+
+  taskData.forEach(
+    ({ id, title, date, description }) => {
+        tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
+        </div>
+      `
+    }
+  );
+};
+
+if (taskData.length){
+    updateTaskContainer()
+}
+
+openTaskFormBtn.addEventListener("click", ()=>{
+    taskForm.classList.toggle("hidden")
+})
+
+closeTaskFormBtn.addEventListener("click", ()=>{
+    if (titleInput.value || dateInput.value || descriptionInput.value){
+        confirmCloseDialog.showModal()
+    }
+    else{
+        reset()
+    }
+})
+
+discardBtn.addEventListener("click", ()=>{
+    confirmCloseDialog.close()
+    reset()
+})
+
+function removeSpecialCharacters(input) {
+  return input.trim().replace(/[^A-Za-z0-9\-\s]/g, '')
+}
+
+taskForm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id)
+
+    const taskObject = {
+        id: removeSpecialCharacters(`${titleInput.value}-${Date.now()}`),
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value,
+    };
+
+    if (dataArrIndex === -1) {
+        taskData.unshift(taskObject);
+  }
+    else {
+        taskData[dataArrIndex] = taskObject;
+  }
+
+    updateTaskContainer()
+    localStorage.setItem("data", JSON.stringify(taskData))
+    reset()
+})
+
+function deleteTask(buttonEl) {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+
+  /* splice already does the work so there is no need in localStorage.removeItem()
+  all that is needed is to save taskData to local Storage oncmeore*/
+  
+  localStorage.setItem("data", JSON.stringify(taskData))
+}
+
+
+
+function editTask(buttonEl) {
+    if (!titleInput.value.trim()){
+        alert("To-do's cannot be only spaces")
+    }
+    const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+
+  taskForm.classList.toggle("hidden");  
+}
